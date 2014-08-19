f55c3b3d267eafa32904a43f3833484ab9378995
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2014-08-19 13:18:14 +0200

    Modified the checker somewhat.

--- application/controllers/charts/ncdichecker.php
+++ application/controllers/charts/ncdichecker.php
@@ -65,29 +65,49 @@ class NCDIChecker extends CI_Controller
 	private function import_csv($filename)
 	{
 		$row = 0;
-		echo implode(",", array('PPnr', 'Percentiel begrip', 'Percentiel productie', 'Taalleeftijd begrip', 'Taalleeftijd productie'));
-		echo '<br>';
+		$out = fopen('php://output', 'w');
+
+		fputcsv($out, array('PPnr', 'Leeftijd in maanden', 'Leeftijd in maanden en dagen', 'Percentiel begrip', 'Percentiel productie', 'Taalleeftijd begrip', 'Taalleeftijd productie'));
 
 		if (($handle = fopen($filename, "r")) !== FALSE)
 		{
-			while (($data = fgetcsv($handle, 1000, ",")) !== FALSE)
+			while (($data = fgetcsv($handle, 1000, ";")) !== FALSE)
 			{
 				$row++;
 				if ($row != 1) // skip header and empties
 				{
-					$ncdi_check = array(
-						'ip_address' 	=> $this->input->ip_address(),
-						'p_number' 		=> $data[0],
-						'ageinmonths' 	=> intval($data[1]),
-						'gender'		=> $data[2] === 'M' ? Gender::Male : Gender::Female,
-						'b_score'		=> intval($data[3]),
-						'p_score'		=> intval($data[4]),				
-					);
-						
+					if (count($data) == 5)
+					{
+						$ncdi_check = array(
+							'ip_address' 	=> $this->input->ip_address(),
+							'p_number' 		=> $data[0],
+							'ageinmonths' 	=> intval($data[1]),
+							'gender'		=> $data[2] === 'M' ? Gender::Male : Gender::Female,
+							'b_score'		=> intval($data[3]),
+							'p_score'		=> intval($data[4]),				
+						);
+					}
+					else
+					{
+						$diff = date_diff(new DateTime($data[1]), new DateTime($data[2]));
+						$ageinmonths = intval($diff->format('%r') . ($diff->format('%m') + 12 * $diff->format('%y')));
+						$ageinmonthsdays = $diff->format('%r') . ($diff->format('%m') + 12 * $diff->format('%y')) . ';' . $diff->format('%d');
+
+						$ncdi_check = array(
+							'ip_address' 	=> $this->input->ip_address(),
+							'p_number' 		=> $data[0],
+							'ageinmonths' 	=> $ageinmonths,
+							'ageinmonthsdays' 	=> $ageinmonthsdays,
+							'gender'		=> $data[3] === 'M' ? Gender::Male : Gender::Female,
+							'b_score'		=> intval($data[4]),
+							'p_score'		=> intval($data[5]),				
+						);
+					}
+
 					$test = $this->testModel->get_test_by_code('ncdi_wz');
 					$testcat_b = $this->testCatModel->get_testcat_by_code($test, 'b');
 					$testcat_p = $this->testCatModel->get_testcat_by_code($test, 'p');
-						
+
 					$perc_b = $this->percentileModel->find_percentile($testcat_b->id,
 					$ncdi_check['gender'], $ncdi_check['ageinmonths'], $ncdi_check['b_score']);
 					$perc_p = $this->percentileModel->find_percentile($testcat_p->id,
@@ -96,14 +116,18 @@ class NCDIChecker extends CI_Controller
 					$ncdi_check['gender'], $ncdi_check['b_score']);
 					$age_p = $this->percentileModel->find_50percentile_age($testcat_p->id,
 					$ncdi_check['gender'], $ncdi_check['p_score']);
-						
-					echo implode(",", array($ncdi_check['p_number'], $perc_b, $perc_p, $age_b, $age_p));
-					echo '<br>';
-						
+
+					fputcsv($out, array($ncdi_check['p_number'], $ncdi_check['ageinmonths'], $ncdi_check['ageinmonthsdays'], $perc_b, $perc_p, $age_b, $age_p));
+
 					//$this->NCDICheckModel->add_ncdi_check($ncdi_check);
 				}
 			}
 			fclose($handle);
 		}
+
+		$filename = 'output.csv';
+
+		$this->load->helper('download');
+		force_download($filename, $out);
 	}
 }
