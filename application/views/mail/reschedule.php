344c883baa1de306a4078e702ddd271f8497bea4
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2014-10-14 16:20:52 +0200

    Reschedule e-mail updated

--- application/views/mail/reschedule.php
+++ application/views/mail/reschedule.php
@@ -1,10 +1,9 @@
 <html>
 <body>
-<!-- TO BE EDITED BY MAARTJE -->
 <p>Beste <?=$name_parent; ?>,</p>
-<p>Uw afspraak is zojuist verzet.</p> 
+<p>Uw afspraak is zojuist verzet naar <strong><?=$appointment; ?></strong>.</p> 
 <p>Meer informatie over het BabyLab en bijvoorbeeld hoe er te komen is beschikbaar op <a href="http://babylab.wp.hum.uu.nl">http://babylab.wp.hum.uu.nl</a>.</p>
-<p>Wij danken u alvast hartelijk voor uw medewerking, zonder uw deelname kunnen wij geen onderzoek doen!</p>
+<p>Wij danken u alvast hartelijk voor uw medewerking. Zonder uw deelname kunnen wij geen onderzoek doen!</p>
 <p><?=BABYLAB_MANAGER; ?> (lab manager)</p>
 <p><em>Deze e-mail is automatisch gegenereerd.</em></p>
 </body>
