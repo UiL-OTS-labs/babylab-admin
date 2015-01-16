de8cab9856cde1f30380b9a2963dbfa4c92a717e
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2015-01-16 09:32:27 +0100

    Fix for duplicate answer rows in survey

--- application/models/surveymodel.php
+++ application/models/surveymodel.php
@@ -27,9 +27,7 @@ class SurveyModel extends CI_Model
 	/** Returns the testinvite for an id */
 	public function get_result_by_token($survey_id, $token)
 	{
-		$this->survey_db->where('token', $token);
-		$this->survey_db->where('submitdate IS NOT NULL');
-		$this->survey_db->get('survey_' . $survey_id)->row();
+		return $this->survey_db->get_where('survey_' . $survey_id, array('token' => $token))->row();
 	}
 
 	/** Returns all question id's and answer values */
