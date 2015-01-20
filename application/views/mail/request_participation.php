b5819247fe8fd0968af055d45abdd6808d8a0efb
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2015-01-20 13:45:55 +0100

    Changed description to experiment name

--- application/views/mail/request_participation.php
+++ application/views/mail/request_participation.php
@@ -5,7 +5,7 @@
 Een van onze medewerkers heeft u tweemaal telefonisch proberen te bereiken (op telefoonnummer <?=$phone; ?>), maar is daarin niet geslaagd. 
 Daarom sturen wij u deze e-mail.</p>
 <p>Op dit moment komt <?=$name_first; ?> in aanmerking om deel te nemen aan een lopend onderzoek. 
-In de bijlage vindt u een korte beschrijving van het onderzoek <em><?=$description; ?></em>, alsmede over onze werkwijze. </p>
+In de bijlage vindt u een korte beschrijving van het onderzoek <em><?=$exp_name; ?></em>, alsmede over onze werkwijze. </p>
 <p>Wij zouden u willen vragen een e-mail terug te sturen aan ons, zodat wij weten of u met <?=$name_first; ?> deel wilt nemen of niet.</p>
 <p>Als u mee wilt doen, zouden wij graag een afspraak plannen tussen <strong><?=$min_date; ?> en <?=$max_date; ?></strong>. 
 Het onderzoek duurt, inclusief uitleg vooraf en ruimte voor vragen achteraf, ongeveer <?=$duration_total; ?> minuten.</p>
