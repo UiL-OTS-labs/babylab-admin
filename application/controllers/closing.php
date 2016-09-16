8eaa28fc8a31aff9e533f100e965b7b11a468732
mhkuu <M.H.vanderKlis@uu.nl>
2016-09-16 11:05:42 +0200

    Started working on editing closings

--- application/controllers/closing.php
+++ application/controllers/closing.php
@@ -2,200 +2,255 @@
 
 class Closing extends CI_Controller
 {
-    public function __construct()
-    {
-        parent::__construct();
-        $this->authenticate->redirect_except();
-        reset_language(current_language());
+	public function __construct()
+	{
+		parent::__construct();
+		$this->authenticate->redirect_except();
+		reset_language(current_language());
 
-        $this->form_validation->set_error_delimiters('<label class="error">', '</label>');
-    }
+		$this->form_validation->set_error_delimiters('<label class="error">', '</label>');
+	}
 
-    public function index($include_past = FALSE)
-    {
-        $add_url = array('url' => 'closing/add', 'title' => lang('add_closing'));
-        $past_url = closing_past_url($include_past);
+	public function index($include_past = FALSE)
+	{
+		$add_url = array('url' => 'closing/add', 'title' => lang('add_closing'));
+		$past_url = closing_past_url($include_past);
 
-        create_closing_table();
-        $data['ajax_source'] = 'closing/table/' . $include_past;
-        $data['page_title'] = lang('closings');
-        $data['action_urls'] = array($add_url, $past_url);
+		create_closing_table();
+		$data['ajax_source'] = 'closing/table/' . $include_past;
+		$data['page_title'] = lang('closings');
+		$data['action_urls'] = array($add_url, $past_url);
 
-        $this->load->view('templates/header', $data);
-        $this->load->view('templates/list_view', $data);
-        $this->load->view('templates/footer');
-    }
+		$this->load->view('templates/header', $data);
+		$this->load->view('templates/list_view', $data);
+		$this->load->view('templates/footer');
+	}
 
-    /** Page to add a closing */
-    public function add()
-    {
-        $data['page_title'] = lang('closings');
-        $data['locations'] = location_options($this->locationModel->get_all_locations());
-        
-        $this->load->view('templates/header', $data);
-        $this->load->view('closing_add_view', $data);
-        $this->load->view('templates/footer');
-    }
+	/** Page to add a closing */
+	public function add()
+	{
+		$data['page_title'] = lang('closings');
+		$data['action'] = 'closing/add_submit';
+		$data = add_fields($data, 'closing');
 
-    /** Adds a closing for a participant */
-    public function add_submit()
-    {
-        // Run validation
-        if (!$this->validate_closing())
-        {
-            // Show form again with error messages
-            $this->add();
-        }
-        else
-        {
-            // If succeeded, insert data into database
-            $closing = $this->post_closing();
+		$data['locations'] = location_options($this->locationModel->get_all_locations());
+		
+		$this->load->view('templates/header', $data);
+		$this->load->view('closing_edit_view', $data);
+		$this->load->view('templates/footer');
+	}
 
-            foreach($closing as $closed)
-            {
-                $this->closingModel->add_closing($closed);
-            }
+	/** Submits the addition of a closing */
+	public function add_submit()
+	{
+		// Run validation
+		if (!$this->validate_closing())
+		{
+			// Show form again with error messages
+			$this->add();
+		}
+		else
+		{
+			// If succeeded, insert data into database
+			$closing = $this->post_closing();
 
-            flashdata(lang('closing_added'));
-            redirect('/closing', 'refresh');
-        }
-    }
+			foreach($closing as $closed)
+			{
+				$this->closingModel->add_closing($closed);
+			}
 
-    /** Deletes the specified closing. */
-    public function delete($closing_id)
-    {
-        $this->closingModel->delete_closing($closing_id);
-        flashdata(lang('closing_deleted'));
-        redirect($this->agent->referrer(), 'refresh');
-    }
+			flashdata(lang('closing_added'));
+			redirect('/closing', 'refresh');
+		}
+	}
 
-    /////////////////////////
-    // Form handling
-    /////////////////////////
+	/** Page to edit a closing */
+	public function edit($closing_id)
+	{
+		$closing = $this->closingModel->get_closing_by_id($closing_id);
 
-    /** Validates an closing */
-    private function validate_closing()
-    {
-        // TODO: validate that either location or lockdown has been selected
-        $this->form_validation->set_rules('location', lang('location'), 'callback_not_zero');
-        $this->form_validation->set_rules('lockdown', lang('lockdown'), 'trim');
-        $this->form_validation->set_rules('all_day', lang('all_day'), 'trim');
-        $this->form_validation->set_rules('from_date', lang('from_date'), 'trim|callback_daterange_required|callback_check_within_bounds');
-        $this->form_validation->set_rules('to_date', lang('to_date'), 'trim|callback_daterange_required|callback_check_within_bounds');
-        $this->form_validation->set_rules('date', lang('date'), 'trim|callback_date_required|callback_check_within_bounds');
-        $this->form_validation->set_rules('comment', lang('comment'), 'trim');
+		$data['page_title'] = lang('closings');
+		$data['action'] = 'closing/edit_submit/' . $closing_id;
+		$data = add_fields($data, 'closing', $closing);
 
-        return $this->form_validation->run();
-    }
+		$data['locations'] = location_options($this->locationModel->get_all_locations());
 
-    /** Posts the data for an closing */
-    private function post_closing()
-    {
-        $postingdata = array();
+		$data['lockdown'] = !$closing->location_id;
+		$data['from'] = output_datetime($closing->from, TRUE);
+		$data['end'] = output_datetime($closing->to, TRUE);
+		$data['all_day'] = FALSE;
+		$data['from'] = output_date($closing->from, TRUE);
+		$data['current_location_id'] = $closing->location_id ? array($closing->location_id) : array(); 
+		
+		$this->load->view('templates/header', $data);
+		$this->load->view('closing_edit_view', $data);
+		$this->load->view('templates/footer');
+	}
 
-        $lockdown   = $this->input->post('lockdown') === '1';
-        $locations  = ($lockdown) ? array(null) : $this->input->post('location');
-        $all_day     = $this->input->post('all_day') === '1';
-        if($all_day){
-            $from       = input_datetime($this->input->post('date'));
-            $to         = input_datetime($this->input->post('date') . " 23:59");
-        } else {
-            $from       = input_datetime($this->input->post('from_date'));
-            $to         = input_datetime($this->input->post('to_date'));
-        }
-        $comment    = $this->input->post('comment');
-        
+	/** Submits the edit of a closing */
+	public function edit_submit($closing_id)
+	{
+		$c = $this->closingModel->get_closing_by_id($closing_id);
 
-        if($locations){
-            foreach($locations as $location)
-            {
-                array_push($postingdata, array(
-                        'location_id'   => $location,
-                        'from'          => $from,
-                        'to'            => $to,
-                        'comment'       => $comment,
-                    ));
-            }    
-        }
-        
-        return $postingdata;
-            
-    }
+		// Run validation
+		if (!$this->validate_closing())
+		{
+			// Show form again with error messages
+			$this->edit();
+		}
+		else
+		{
+			// If succeeded, insert data into database
+			$closing = $this->post_closing();
 
-    /////////////////////////
-    // Callbacks
-    /////////////////////////
+			foreach ($closing as $closed)
+			{
+				$this->closingModel->add_closing($closed);
+			}
 
-    /** Checks whether the given date is within bounds of an existing closing for this location */
-    public function check_within_bounds($date)
-    {
-        $locations = $this->input->post('lockdown') === '1' ? array(null) : $this->input->post('location');
-        if($locations)
-        {
-            foreach($locations as $location_id)
-            {
-                // $location_id = $this->input->post('location');
-                if ($this->closingModel->within_bounds(input_datetime($date), $location_id))
-                {
-                    $this->form_validation->set_message('check_within_bounds', lang('closing_within_bounds'));
-                    return FALSE;
-                }
-            }
-        }
-        
-        return TRUE;
-    }
+			flashdata(lang('closing_added'));
+			redirect('/closing', 'refresh');
+		}
+	}
 
-    /** Checks whether the given parameter is valid (if at least one location is set) */
-    public function not_zero($values)
-    {
-        if (!$values && $this->input->post('lockdown') !== '1')
-        {
-            $this->form_validation->set_message('not_zero', lang('isset'));
-            return FALSE;
-        }
-        return TRUE;
-    }
+	/** Deletes the specified closing. */
+	public function delete($closing_id)
+	{
+		$this->closingModel->delete_closing($closing_id);
+		flashdata(lang('closing_deleted'));
+		redirect($this->agent->referrer(), 'refresh');
+	}
 
-    public function daterange_required($values)
-    {
-        if(!$values && $this->input->post('all_day') !== '1')
-        {
-            $this->form_validation->set_message('daterange_required', lang('isset'));
-            return FALSE;
-        }
-        return TRUE;
-    }
+	/////////////////////////
+	// Form handling
+	/////////////////////////
 
-    public function date_required($values)
-    {
-        if(!$values && $this->input->post('all_day') === '1')
-        {
-            $this->form_validation->set_message('date_required', lang('isset'));
-            return FALSE;
-        }
-        return TRUE;
-    }
+	/** Validates an closing */
+	private function validate_closing()
+	{
+		// TODO: validate that either location or lockdown has been selected
+		$this->form_validation->set_rules('location', lang('location'), 'callback_not_zero');
+		$this->form_validation->set_rules('lockdown', lang('lockdown'), 'trim');
+		$this->form_validation->set_rules('all_day', lang('all_day'), 'trim');
+		$this->form_validation->set_rules('from_date', lang('from_date'), 'trim|callback_daterange_required|callback_check_within_bounds');
+		$this->form_validation->set_rules('to_date', lang('to_date'), 'trim|callback_daterange_required|callback_check_within_bounds');
+		$this->form_validation->set_rules('date', lang('date'), 'trim|callback_date_required|callback_check_within_bounds');
+		$this->form_validation->set_rules('comment', lang('comment'), 'trim');
 
-    /////////////////////////
-    // Table
-    /////////////////////////
-    
-    public function table($include_past = FALSE)
-    {
-        $this->datatables->select('name, from, comment, closing.id AS id, location_id');
-        $this->datatables->from('closing');
-        $this->datatables->join('location', 'location.id = closing.location_id', 'LEFT');
+		return $this->form_validation->run();
+	}
 
-        if (!$include_past) $this->db->where('to >=', input_date());
+	/** Posts the data for an closing */
+	private function post_closing()
+	{
+		$postingdata = array();
 
-        $this->datatables->edit_column('name', '$1', 'closing_location_link_by_id(location_id)');
-        $this->datatables->edit_column('from', '$1', 'closing_dates_by_id(id)');
-        $this->datatables->edit_column('comment', '$1', 'comment_body(comment, 30)');
-        $this->datatables->edit_column('id', '$1', 'closing_actions(id)');
+		$lockdown = $this->input->post('lockdown') === '1';
+		$locations = ($lockdown) ? array(null) : $this->input->post('location');
+		$all_day = $this->input->post('all_day') === '1';
+		if ($all_day)
+		{
+			$from = input_datetime($this->input->post('date'));
+			$to   = input_datetime($this->input->post('date') . " 23:59");
+		} 
+		else
+		{
+			$from = input_datetime($this->input->post('from_date'));
+			$to   = input_datetime($this->input->post('to_date'));
+		}
+		$comment = $this->input->post('comment');
+		
 
-        $this->datatables->unset_column('location_id');
+		if ($locations) {
+			foreach ($locations as $location)
+			{
+				array_push($postingdata, array(
+						'location_id'   => $location,
+						'from'          => $from,
+						'to'            => $to,
+						'comment'       => $comment,
+					));
+			}
+		}
+		
+		return $postingdata;
+			
+	}
 
-        echo $this->datatables->generate();
-    }
+	/////////////////////////
+	// Callbacks
+	/////////////////////////
+
+	/** Checks whether the given date is within bounds of an existing closing for this location */
+	public function check_within_bounds($date)
+	{
+		$locations = $this->input->post('lockdown') === '1' ? array(null) : $this->input->post('location');
+		if($locations)
+		{
+			foreach($locations as $location_id)
+			{
+				// $location_id = $this->input->post('location');
+				if ($this->closingModel->within_bounds(input_datetime($date), $location_id))
+				{
+					$this->form_validation->set_message('check_within_bounds', lang('closing_within_bounds'));
+					return FALSE;
+				}
+			}
+		}
+		
+		return TRUE;
+	}
+
+	/** Checks whether the given parameter is valid (if at least one location is set) */
+	public function not_zero($values)
+	{
+		if (!$values && $this->input->post('lockdown') !== '1')
+		{
+			$this->form_validation->set_message('not_zero', lang('isset'));
+			return FALSE;
+		}
+		return TRUE;
+	}
+
+	public function daterange_required($values)
+	{
+		if(!$values && $this->input->post('all_day') !== '1')
+		{
+			$this->form_validation->set_message('daterange_required', lang('isset'));
+			return FALSE;
+		}
+		return TRUE;
+	}
+
+	public function date_required($values)
+	{
+		if(!$values && $this->input->post('all_day') === '1')
+		{
+			$this->form_validation->set_message('date_required', lang('isset'));
+			return FALSE;
+		}
+		return TRUE;
+	}
+
+	/////////////////////////
+	// Table
+	/////////////////////////
+	
+	public function table($include_past = FALSE)
+	{
+		$this->datatables->select('name, from, comment, closing.id AS id, location_id');
+		$this->datatables->from('closing');
+		$this->datatables->join('location', 'location.id = closing.location_id', 'LEFT');
+
+		if (!$include_past) $this->db->where('to >=', input_date());
+
+		$this->datatables->edit_column('name', '$1', 'closing_location_link_by_id(location_id)');
+		$this->datatables->edit_column('from', '$1', 'closing_dates_by_id(id)');
+		$this->datatables->edit_column('comment', '$1', 'comment_body(comment, 30)');
+		$this->datatables->edit_column('id', '$1', 'closing_actions(id)');
+
+		$this->datatables->unset_column('location_id');
+
+		echo $this->datatables->generate();
+	}
 }
--- application/helpers/closing_helper.php
+++ application/helpers/closing_helper.php
@@ -6,65 +6,67 @@
 
 if (!function_exists('create_closing_table'))
 {
-    /** Creates the table with closing data */
-    function create_closing_table($id = NULL)
-    {
-        $CI =& get_instance();
-        base_table($id);
-        $CI->table->set_heading(lang('location'), lang('closing'), lang('comment'), lang('actions'));
-    }
+	/** Creates the table with closing data */
+	function create_closing_table($id = NULL)
+	{
+		$CI =& get_instance();
+		base_table($id);
+		$CI->table->set_heading(lang('location'), lang('closing'), lang('comment'), lang('actions'));
+	}
 }
 
 if (!function_exists('closing_dates'))
 {
-    /** Gets the closing dates in readable format */
-    function closing_dates($closing)
-    {
-        return output_datetime($closing->from) . ' - ' . output_datetime($closing->to);
-    }
+	/** Gets the closing dates in readable format */
+	function closing_dates($closing)
+	{
+		return output_datetime($closing->from) . ' - ' . output_datetime($closing->to);
+	}
 }
 
 if (!function_exists('closing_dates_by_id'))
 {
-    /** Gets the closing dates in readable format */
-    function closing_dates_by_id($closing_id)
-    {
-        $CI =& get_instance();
-        $closing = $CI->closingModel->get_closing_by_id($closing_id);
+	/** Gets the closing dates in readable format */
+	function closing_dates_by_id($closing_id)
+	{
+		$CI =& get_instance();
+		$closing = $CI->closingModel->get_closing_by_id($closing_id);
 
-        return closing_dates($closing);
-    }
+		return closing_dates($closing);
+	}
 }
 
 if (!function_exists('closing_actions'))
 {
-    /** Possible actions for an closing: delete */
-    function closing_actions($closing_id)
-    {
-        return anchor('closing/delete/' . $closing_id, img_delete(), warning(lang('sure_delete_closing')));
-    }
+	/** Possible actions for an closing: delete */
+	function closing_actions($closing_id)
+	{
+		$edit = anchor('closing/edit/' . $closing_id, img_edit());
+		$delete = anchor('closing/delete/' . $closing_id, img_delete(), warning(lang('sure_delete_closing')));
+		return implode(' ', array($edit, $delete));
+	}
 }
 
 if (!function_exists('closing_past_url'))
 {
-    /** The "past" url for an closing */
-    function closing_past_url($include_past)
-    {
-        $include_past_url = array('url' => 'closing/index/1', 'title' => lang('closing_include_past'));
-        $dont_include_past_url = array( 'url' => 'closing/index/0', 'title' => lang('closing_exclude_past'));
+	/** The "past" url for an closing */
+	function closing_past_url($include_past)
+	{
+		$include_past_url = array('url' => 'closing/index/1', 'title' => lang('closing_include_past'));
+		$dont_include_past_url = array( 'url' => 'closing/index/0', 'title' => lang('closing_exclude_past'));
 
-        return $include_past ? $dont_include_past_url : $include_past_url;
-    }
+		return $include_past ? $dont_include_past_url : $include_past_url;
+	}
 }
 
 if (!function_exists('closing_location_link_by_id'))
 {
-    function closing_location_link_by_id($location_id)
-    {
-         if(isset($location_id)){
-            return location_get_link_by_id($location_id);
-        } else {
-            return lang('lockdown');
-        }
-    }
+	function closing_location_link_by_id($location_id)
+	{
+		 if(isset($location_id)){
+			return location_get_link_by_id($location_id);
+		} else {
+			return lang('lockdown');
+		}
+	}
 }
