d8322366b0cb1e8becaec7ab399003a50aa3ee55
mhkuu <M.H.vanderKlis@uu.nl>
2016-09-28 15:47:36 +0200

    Fixes for e-mail text

--- application/views/mail/ncdi_short_completion.php
+++ application/views/mail/ncdi_short_completion.php
@@ -1,11 +1,12 @@
 <html>
 <body>
 <p>Geachte heer/mevrouw,</p>
-<p>Via onze website heeft u een vragenlijst ingevuld over het taalbegrip van uw kind. Hieruit is een score gekomen waarmee u de resultaten van uw kind kunt vergelijken met de resultaten van andere kinderen van dezelfde leeftijd. Zo ziet u waar op dit moment de taalvaardigheid van uw kind valt ten opzichte van het gemiddelde.</p>
+<p>Via onze website heeft u een vragenlijst ingevuld over de woordenschat van uw kind. Hieruit is een persoonlijke score gekomen over de huidige taalvaardigheid van uw kind.</p>
+<p>Door middel van de percentielscores kunt u de resultaten van uw kind vergelijken met de resultaten van andere kinderen van dezelfde leeftijd en hetzelfde geslacht. Een percentielscore van 50 betekent dat 50% van de kinderen een lagere score behaalde dan uw kind. Dit is dus het gemiddelde. Let wel, de lijst die u hebt ingevuld is een zeer korte variant van een veel uitgebreidere woordenschatlijst. De resultaten geven dan ook alleen een (grove) indicatie van de woordenschat van uw kind.</p>
 <p>Door op onderstaande link te klikken ziet u de uitslag van de door u ingevulde vragenlijst:</p>
 <p><?=$results_link; ?></p>
-<p>Wij willen u met nadruk laten weten dat wij zeer vertrouwelijk omgaan met de informatie die u ons toevertrouwt. Bovendien wordt de lijst op een veilige manier bewaard: de informatie is alleen toegankelijk voor de desbetreffende onderzoeker.</p>
-<p>Mocht u vragen hebben n.a.v. de vragenlijst of de interpretatie daarvan, dan kunt u contact opnemen met <?=BABYLAB_MANAGER; ?>: <?=BABYLAB_MANAGER_PHONE; ?> of mailen naar: <?=mailto(BABYLAB_MANAGER_EMAIL); ?>.</p>
+<p>Wij willen u met nadruk laten weten dat alle gegevens volledig geanonimiseerd worden opgeslagen. Bovendien wordt de lijst op een veilige server van de Universiteit Utrecht bewaard, welke alleen toegankelijk is voor de babylabmanager.</p>
+<p>Mocht u vragen hebben n.a.v. de vragenlijst of de interpretatie daarvan, dan kunt u contact met ons opnemen door te mailen naar <?=mailto(BABYLAB_MANAGER_EMAIL); ?>.</p>
 <p>Hartelijke groet,</p>
 <p><?=BABYLAB_TEAM; ?></p>
 <p><em>Deze e-mail is automatisch gegenereerd.</em></p>
