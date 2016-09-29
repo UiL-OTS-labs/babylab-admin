710d40b754c796746bcfe6fbdcd113228ec4aa05
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2016-09-29 12:02:41 +0200

    Small change to completion mail

--- application/views/mail/ncdi_short_completion.php
+++ application/views/mail/ncdi_short_completion.php
@@ -1,13 +1,12 @@
 <html>
 <body>
 <p>Geachte heer/mevrouw,</p>
-<p>Via onze website heeft u een vragenlijst ingevuld over de woordenschat van uw kind. Hieruit is een persoonlijke score gekomen over de huidige taalvaardigheid van uw kind.</p>
-<p>Door middel van de percentielscores kunt u de resultaten van uw kind vergelijken met de resultaten van andere kinderen van dezelfde leeftijd en hetzelfde geslacht. Een percentielscore van 50 betekent dat 50% van de kinderen een lagere score behaalde dan uw kind. Dit is dus het gemiddelde. Let wel, de lijst die u hebt ingevuld is een zeer korte variant van een veel uitgebreidere woordenschatlijst. De resultaten geven dan ook alleen een (grove) indicatie van de woordenschat van uw kind.</p>
-<p>Door op onderstaande link te klikken ziet u de uitslag van de door u ingevulde vragenlijst:</p>
+<p>Via onze website heeft u een vragenlijst ingevuld over de woordenschat van uw kind. Hieruit is een persoonlijke score gekomen over de huidige woordenschat van uw kind. Door op onderstaande link te klikken ziet u de uitslag van de door u ingevulde vragenlijst:</p>
 <p><?=$results_link; ?></p>
-<p>Wij willen u met nadruk laten weten dat alle gegevens volledig geanonimiseerd worden opgeslagen. Bovendien wordt de lijst op een veilige server van de Universiteit Utrecht bewaard, welke alleen toegankelijk is voor de babylabmanager.</p>
+<p>Door middel van de percentielscores kunt u de resultaten van uw kind vergelijken met de resultaten van andere kinderen van dezelfde leeftijd en hetzelfde geslacht. Een percentielscore van 50 betekent dat 50% van de kinderen een lagere score behaalde dan uw kind. Dit is dus het gemiddelde. <strong>Let wel</strong>, de lijst die u hebt ingevuld is een zeer korte variant van een veel uitgebreidere woordenschatlijst. De resultaten geven dan ook alleen een (grove) indicatie van de woordenschat van uw kind.</p>
+<p>Uw antwoorden zijn automatisch opgeslagen op een beveiligde server van de Universiteit Utrecht. Wanneer u akkoord bent gegaan met het gebruik van uw gegevens voor wetenschappelijke doeleinden, zullen uw gegevens volledig geanonimiseerd verwerkt worden.</p>
 <p>Mocht u vragen hebben n.a.v. de vragenlijst of de interpretatie daarvan, dan kunt u contact met ons opnemen door te mailen naar <?=mailto(BABYLAB_MANAGER_EMAIL); ?>.</p>
-<p>Hartelijke groet,</p>
+<p>Met vriendelijke groet,</p>
 <p><?=BABYLAB_TEAM; ?></p>
 <p><em>Deze e-mail is automatisch gegenereerd.</em></p>
 </body>
