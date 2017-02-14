28c409de74ea6817cd231eafa8c2ad088cf69810
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2017-02-14 17:43:58 +0100

    New email for HVF follow-up survey

---
+++ application/views/mail/hvf_followup.php
@@ -0,0 +1,20 @@
+<html>
+<body>
+<p>Beste <?=$name_parent; ?>,</p>
+<p>Binnenkort gaat u met uw <?=$gender; ?> <?=$name_first; ?> deelnemen aan een onderzoek aan het BabyLab van de Universiteit Utrecht.
+Hier zijn we heel blij mee, zo kunnen vragen op het gebied van taalverwerving bij kinderen beantwoord worden.</p>
+<p>Bij het onderzoek waar u aan mee gaat doen, hoort ook het invullen van een vragenlijst over de achtergrond en ontwikkeling van <?=$name_first; ?>.
+Om deze reden sturen wij u deze e-mail.
+Wij zouden u dan ook willen vragen of u de link naar de vragenlijst aan het einde van deze e-mail wilt openen.
+Het invullen duurt enige tijd.
+Daar wijzen wij u op, zodat u een goed moment kunt kiezen om de vragen te beantwoorden.
+<p>Wij willen u met nadruk laten weten dat wij zeer vertrouwelijk omgaan met de informatie die u ons toevertrouwt.
+Bovendien wordt de lijst op een veilige manier bewaard: de informatie is alleen toegankelijk voor de desbetreffende onderzoeker.</p>
+<p>De link naar de vragenlijst (<em><?=$survey_name; ?></em>) vindt u hier: <?=$survey_link; ?>.</p>
+<p>Mocht u vragen hebben n.a.v. de vragenlijst, dan kunt u contact met ons opnemen door te mailen naar <?=mailto(BABYLAB_MANAGER_EMAIL); ?>.</p>
+<p>Wij danken u alvast hartelijk voor uw medewerking, zonder uw deelname kunnen wij geen onderzoek doen!</p>
+<p>Hartelijke groet,</p>
+<p><?=BABYLAB_TEAM; ?></p>
+<p><em>Deze e-mail is automatisch gegenereerd.</em></p>
+</body>
+</html>
