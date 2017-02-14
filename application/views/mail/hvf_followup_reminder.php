b13e8251be779e2db4af5586457592504c5686d2
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2017-02-14 18:12:53 +0100

    Also creating the reminder e-mail

---
+++ application/views/mail/hvf_followup_reminder.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+<p>Beste <?=$name_parent; ?>,</p>
+<p>Graag willen wij u eraan herinneren dat wij u onlangs een e-mail hebben gestuurd met het verzoek een korte vragenlijst in te vullen.</p>
+<p>De link naar de vragenlijst (<em><?=$survey_name; ?></em>) vindt u hier: <?=$survey_link; ?>.</p>
+<p>Mocht u vragen hebben n.a.v. deze e-mail of de vragenlijst, dan kunt u contact met ons opnemen door te mailen naar <?=mailto(BABYLAB_MANAGER_EMAIL); ?>.</p>
+<p>Wij danken u alvast hartelijk voor uw medewerking, zonder uw deelname kunnen wij geen onderzoek doen!</p>
+<p>Hartelijke groet,</p>
+<p><?=BABYLAB_TEAM; ?></p>
+<p><em>Deze e-mail is automatisch gegenereerd.</em></p>
+</body>
+</html>
