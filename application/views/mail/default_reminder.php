e75df817dde465fab42bc8c4574acaf43d5d60d4
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2017-02-23 09:04:22 +0100

    Created default invitation and reminder from hvf_followup

--- application/views/mail/default_reminder.php
+++ application/views/mail/default_reminder.php
@@ -1,7 +1,7 @@
 <html>
 <body>
 <p>Beste <?=$name_parent; ?>,</p>
-<p>Graag willen wij u eraan herinneren dat wij u onlangs een e-mail hebben gestuurd met het verzoek een korte vragenlijst in te vullen.</p>
+<p>Graag willen wij u eraan herinneren dat wij u onlangs een e-mail hebben gestuurd met het verzoek een vragenlijst in te vullen.</p>
 <p>De link naar de vragenlijst (<em><?=$survey_name; ?></em>) vindt u hier: <?=$survey_link; ?>.</p>
 <p>Mocht u vragen hebben n.a.v. deze e-mail of de vragenlijst, dan kunt u contact met ons opnemen door te mailen naar <?=mailto(BABYLAB_MANAGER_EMAIL); ?>.</p>
 <p>Wij danken u alvast hartelijk voor uw medewerking, zonder uw deelname kunnen wij geen onderzoek doen!</p>
