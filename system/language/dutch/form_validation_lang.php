9a56743e67257bc6bdabae2d585a7b99ddd10b08
Ty Mees <t.d.mees@uu.nl>
2019-09-10 14:13:04 +0200

    Updated CI to 3.1.10

--- system/language/dutch/form_validation_lang.php
+++ system/language/dutch/form_validation_lang.php
@@ -1,26 +1,41 @@
 <?php
+/**
+ * System messages translation for CodeIgniter(tm)
+ *
+ * @author	CodeIgniter community
+ * @copyright	Copyright (c) 2014-2018, British Columbia Institute of Technology (http://bcit.ca/)
+ * @copyright	Pieter Krul
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	https://codeigniter.com
+ */
+defined('BASEPATH') OR exit('Directe toegang tot scripts is niet toegestaan');
 
-$lang['required'] 			= 'Het veld "%s" mag niet leeg zijn.';
-$lang['isset'] 				= 'Het veld "%s" moet een waarde hebben.';
-$lang['valid_email'] 		= 'Het veld "%s" moet een geldig e-mailadres bevatten.';
-$lang['valid_emails'] 		= 'Het veld "%s" moet geldige e-mailadressen bevatten.';
-$lang['valid_url']			= 'Het veld "%s" moet een correcte URL bevatten.';
-$lang['valid_ip'] 			= 'Het veld "%s" moet een correct IP adres bevatten.';
-$lang['min_length'] 		= 'Het veld "%s" moet tenminste %s tekens bevatten.';
-$lang['max_length'] 		= 'Het veld "%s" mag niet meer dan %s tekens bevatten.';
-$lang['exact_length'] 		= 'Het veld "%s" moet precies %s tekens bevatten.';
-$lang['alpha'] 				= 'Het veld "%s" mag alleen letters bevatten.';
-$lang['alpha_numeric'] 		= 'Het veld "%s" mag alleen letters en cijfers bevatten.';
-$lang['alpha_dash'] 		= 'Het veld "%s" mag alleen letters, cijfers, underscores(_) en strepen(-) bevatten.';
-$lang['numeric'] 			= 'Het veld "%s" mag alleen een getal bevatten.';
-$lang['is_numeric'] 		= 'Het veld "%s" mag alleen een getal bevatten.';
-$lang['integer'] 			= 'Het veld "%s" moet een geheel getal bevatten.';
-$lang['regex_match']		= 'Het veld "%s" is niet in het juiste formaat.';
-$lang['matches'] 			= 'De waarde van het veld "%s" is niet gelijk aan de waarde van het veld "%s".';
-$lang['is_unique'] 			= 'Het veld "%s" moet een unieke waarde bevatten.';
-$lang['is_natural'] 		= 'Het veld "%s" mag alleen een getal bevatten dat niet kleiner is dan nul.';
-$lang['is_natural_no_zero'] = 'Het veld "%s" moet een getal bevatten dat groter is dan nul.';
-$lang['decimal']			= 'Het veld "%s" mag alleen een decimaal nummer bevatten.';
-$lang['less_than']			= 'Het veld "%s" mag alleen een getal bevatten dat kleiner is dan %s.';
-$lang['greater_than']		= 'Het veld "%s" mag alleen een getal bevatten dat groter is dan %s.';
-?>
+$lang['form_validation_required']		= 'Het {field}-veld is verplicht.';
+$lang['form_validation_isset']			= 'Het {field}-veld mag niet leeg zijn.';
+$lang['form_validation_valid_email']		= 'Het {field}-veld dient een geldig e-mailadres te bevatten.';
+$lang['form_validation_valid_emails']		= 'Het {field}-veld dient geldige e-mailadressen te bevatten.';
+$lang['form_validation_valid_url']		= 'Het {field}-veld dient een geldige URL te bevatten.';
+$lang['form_validation_valid_ip']		= 'Het {field}-veld dient een geldig IP-adres te bevatten.';
+$lang['form_validation_min_length']		= 'Het {field}-veld dient tenminste {param} karakters lang te zijn.';
+$lang['form_validation_max_length']		= 'Het {field}-veld mag niet meer dan {param} karaktertekens bevatten.';
+$lang['form_validation_exact_length']		= 'Het {field}-veld dient precies {param} karaktertekens te bevatten.';
+$lang['form_validation_alpha']			= 'Het {field}-veld mag alleen alfabetische karaktertekens bevatten.';
+$lang['form_validation_alpha_numeric']		= 'Het {field}-veld mag alleen alfanumerieke karaktertekens bevatten.';
+$lang['form_validation_alpha_numeric_spaces']	= 'Het {field}-veld mag alleen alfanumerieke karaktertekens en spaties bevatten.';
+$lang['form_validation_alpha_dash']		= 'Het {field}-veld mag alleen alfanumerieke karaktertekens, underscores, en het minteken bevatten.';
+$lang['form_validation_numeric']		= 'Het {field}-veld mag alleen cijfers bevatten.';
+$lang['form_validation_is_numeric']		= 'Het {field}-veld mag alleen numerieke waarden bevatten.';
+$lang['form_validation_integer']		= 'Het {field}-veld mag alleen hele getallen bevatten.';
+$lang['form_validation_regex_match']		= 'Het {field}-veld heeft niet het juiste formaat.';
+$lang['form_validation_matches']		= 'Het {field}-veld komt niet overeen met het {param}-veld.';
+$lang['form_validation_differs']		= 'Het {field}-veld dient verschillend te zijn van het {param}-veld.';
+$lang['form_validation_is_unique']		= 'Het {field}-veld dient een unieke waarde te bevatten.';
+$lang['form_validation_is_natural']		= 'Het {field}-veld dient alleen cijfers te bevatten.';
+$lang['form_validation_is_natural_no_zero']	= 'Het {field}-veld dient alleen cijfers groter dan nul te bevatten.';
+$lang['form_validation_decimal']		= 'Het {field}-veld dient een decimaal nummer te bevatten.';
+$lang['form_validation_less_than']		= 'Het {field}-veld dient een cijfer kleiner dan {param} te bevatten.';
+$lang['form_validation_less_than_equal_to']	= 'Het {field}-veld dient een cijfer kleiner of gelijk aan {param} te bevatten.';
+$lang['form_validation_greater_than']		= 'Het {field}-veld dient een cijfer groter dan {param} te bevatten.';
+$lang['form_validation_greater_than_equal_to']	= 'Het {field}-veld dient een cijfer groter dan, of gelijk aan {param} te bevatten.';
+$lang['form_validation_error_message_not_set']	= 'Tijdens de validatie van het {field}-veld is een niet nader genoemde fout opgetreden.';
+$lang['form_validation_in_list']		= 'Het {field}-veld moet één van de volgende waardes bevatten: {param}.';
--- system/language/dutch/pagination_lang.php
+++ system/language/dutch/pagination_lang.php
@@ -10,8 +10,7 @@
  */
 defined('BASEPATH') OR exit('Directe toegang tot scripts is niet toegestaan');
 
-$lang['terabyte_abbr']	= 'TB';
-$lang['gigabyte_abbr']	= 'GB';
-$lang['megabyte_abbr']	= 'MB';
-$lang['kilobyte_abbr']	= 'kB';
-$lang['bytes']		= 'Bytes';
+$lang['pagination_first_link']	= '&lsaquo; Eerste';
+$lang['pagination_next_link']	= '&gt;';
+$lang['pagination_prev_link']	= '&lt;';
+$lang['pagination_last_link']	= 'Laatste &rsaquo;';
