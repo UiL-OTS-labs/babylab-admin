5b48e6722f122dbdd01eac13b1f063ff1ce727b1
Chris Rauws <c.m.rauws@uu.nl>
2020-08-25 13:58:20 +0100

    Tekstuele wijzinging in danktekst na invullen survey

--- application/views/anam/home.php
+++ application/views/anam/home.php
@@ -1,5 +1,5 @@
 <?php if ($valid_token) { ?>
-	<?=heading('Hartelijk bedankt!', 2); ?>
+	<?=heading('Hartelijk dank voor het invullen van deze vragenlijst.', 2); ?>
 	<p>
 		Hartelijk dank voor het invullen van de vragenlijst <em><?=$test_name; ?></em>. 
 		Uw antwoorden zijn opgeslagen en worden met de grootste zorg behandeld.
