601597588a90c01392a4e38c3fb34be6d5aee8aa
Mees, T.D. (Ty) <t.d.mees@uu.nl>
2022-11-21 16:15:41 +0100

    Added missing period

--- application/views/mail/confirmation.php
+++ application/views/mail/confirmation.php
@@ -13,7 +13,7 @@
         <?php } else { ?>
         U en <?=$name; ?> worden verwacht op <strong><?=$appointment; ?></strong> op Janskerkhof <strong>13a</strong>
         (let op: dit is de groene voordeur met de helling ervoor), zie
-        <?=anchor('https://babylab.wp.hum.uu.nl/route-in-het-gebouw-13a/', 'https://babylab.wp.hum.uu.nl/route-in-het-gebouw-13a/'); ?>. Als u aanbelt en via de intercom zegt dat u voor het Babylab komt, dan wordt de deur voor u geopend
+        <?=anchor('https://babylab.wp.hum.uu.nl/route-in-het-gebouw-13a/', 'https://babylab.wp.hum.uu.nl/route-in-het-gebouw-13a/'); ?>. Als u aanbelt en via de intercom zegt dat u voor het Babylab komt, dan wordt de deur voor u geopend.
         <?php } ?>
         Eventuele reis- en parkeerkosten worden vergoed zoals aangegeven in de bijgevoegde informatiebrief.
     </p>
