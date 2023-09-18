65843efe47ad55c3a22cb12db116a7fd767228f8
Ben <b.bonfil@uu.nl>
2023-09-18 09:34:15 +0200

    updated text in confrimation and reminder emails

--- application/views/mail/reminder.php
+++ application/views/mail/reminder.php
@@ -5,7 +5,7 @@
 Wij willen u eraan herinneren dat dit experiment morgen plaatsvindt: <strong><?=$appointment; ?></strong> op het <strong>Janskerkhof 13a</strong>.</p>
 <p>Informatie over het onderzoek kunt u terugvinden in de e-mail die wij ter bevestiging hebben gestuurd na het maken van de afspraak.</p>
 <p>Als u bij de bevestiging ook een vragenlijst heeft ontvangen, kunt u dan zorgen dat u deze ingevuld heeft voor uw bezoek? Alvast bedankt!</p>
-<p>Wanneer u verhinderd bent te komen of de afspraak graag wil verzetten, dan kunt u mailen naar: <?=mailto(BABYLAB_MANAGER_EMAIL); ?>.
+<p>Als uw kind op dit moment oorontsteking heeft en dus mogelijk minder goed hoort, dan willen wij (indien mogelijk) de afspraak graag verzetten. Wanneer u verhinderd bent te komen of de afspraak graag wil verzetten, dan kunt u mailen naar: <?=mailto(BABYLAB_MANAGER_EMAIL); ?>.
 Mocht u verder nog vragen hebben dan kunt u contact opnemen met:</p>
 <?=ul($caller_contacts); ?>
 <p>Meer informatie over het Babylab en bijvoorbeeld hoe er te komen is beschikbaar op <a href="https://babylab.wp.hum.uu.nl">https://babylab.wp.hum.uu.nl</a>.</p>
