102972e47253dd2cced2d86d70078129a23a1e2d
Mees, T.D. (Ty) <t.d.mees@uu.nl>
2023-10-16 15:22:56 +0200

    fix class name

--- application/migrations/041_update_participant.php
+++ application/migrations/041_update_participant.php
@@ -2,7 +2,7 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Migration_Update_Participation extends CI_Migration
+class Migration_Update_participant extends CI_Migration
 {
 
 	public function up()
